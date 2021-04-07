2e4ca48153e8ab6d49ca29664a2f744b1186143d
sugar-ryota <s1811345@s.tsukuba.ac.jp>
2021-04-07 12:33:32 +0900

    pdfからcsvファイルに変換するようにした

--- TITC/TableImageToCsv/views.py
+++ TITC/TableImageToCsv/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse
 from .forms import UploadFileForm
-from django.views.generic import TemplateView
-# 画像処理ライブラリのimport
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.backends.backend_pdf import PdfPages
+
 import pandas as pd
 
 #pdfからcsvに変換するためのライブラリ
@@ -18,44 +13,27 @@ def fileUpload(request):
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
       file_obj = request.FILES['file']
-      # handle_uploaded_file(file_obj)
-      df = toPdf(file_obj)
-      context = { 'df' : df}
+      handle_uploaded_file(file_obj)
+      toCsv(file_obj)
+      context = {
+         'success': '成功！',
+         }
       return render(request,'TableImageToCsv/result.html',context)
   else:
     form = UploadFileForm()
-  return render(request,'TableImageToCsv/index.html',{'form':form})
+  return render(request, 'TableImageToCsv/index.html', {'form': form})
 
-# アップロードされた画像を保存する関数
+# 受け取ったpdfファイルを保存する関数
 def handle_uploaded_file(file_obj):
-  file_path = 'media/documents/' + file_obj.name
+  file_path = file_obj.name
   with open(file_path, 'wb+') as destination:
     for chunk in file_obj.chunks():
       destination.write(chunk)
 
-# 画像をpdfにする関数
-def toPdf(file_obj):
-  # 画像をファイルから読み込む
-  image = Image.open(file_obj)
-  # 画像をNumpy配列に変換する
-  image = np.asarray(image)
-
-
-  # 画像のプロット先の準備
-  fig = plt.figure()
-  # グリッドの表示をOFFにする
-  plt.axis('off')
-  # Numpy配列を画像として表示する
-  plt.imshow(image)
-
-  # 保存するPDFファイル名
-  pp = PdfPages(file_obj.name.split('.')[0]+'.pdf')
-  # 画像をPDFとして保存する
-  pp.savefig(fig)
-  # PDFの保存終了
-  pp.close()
+# 表pdfをcsvにする関数
+def toCsv(file_obj):
   file_name = file_obj.name.split('.')[0] + '.pdf'
-
   df = read_pdf(file_name)
-  pd.DataFrame(df[0]).to_csv(file_obj.name.split('.')[0]+'.csv',encoding='shift-jis')
-  return df
+  csv_file = file_obj.name.split('.')[0] + '.csv'
+  # １つのページに複数の表があるときはdfの配列番号を指定すると良い
+  pd.DataFrame(df[0]).to_csv(csv_file, encoding='shift-jis')
